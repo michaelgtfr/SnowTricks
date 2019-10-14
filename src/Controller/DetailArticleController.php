7effb7b45dd394eae0a453ffbaaed09f37210242
mickdu62 <mickdu62200@gmail.com>
2019-10-14 16:15:34 +0200

    create the detailArticle class, retrieve the requested element from the Bdd

--- src/Controller/DetailArticleController.php
+++ src/Controller/DetailArticleController.php
@@ -3,18 +3,35 @@
 namespace App\Controller;
 
 
+use App\Entity\Item;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DetailArticleController extends AbstractController
 {
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      *
-     * @Route("/detail/{{$id}}", name="app_detail")
+     * @Route("/detail/{id}", name="app_detail")
      */
-    public function detailArticle()
+    public function detailArticle(Request $request, EntityManagerInterface $em )
     {
-        return $this->render('detailArticle/detailArticle.html.twig', []);
+        $item = $em->getRepository(Item::class)
+                    ->find($request->get('id'));
+
+        $pictures = $item->getPictures();
+
+        $movies = $item->getMovies();
+
+        $comments = $item->getComments();
+
+        return $this->render('detailArticle/detailArticle.html.twig', [
+            'item' => $item,
+            'pictures' => $pictures,
+            'movies' => $movies,
+            'comments' => $comments
+        ]);
     }
 }
