fc7e617948bc601b4541d81e0d9d2f2793db5744
mickdu62 <mickdu62200@gmail.com>
2020-01-17 07:01:39 +0100

    amendment as recommended codacy

--- public/js/functionJs/addPictureAsynchronous.js
+++ public/js/functionJs/addPictureAsynchronous.js
@@ -4,16 +4,16 @@ jQuery(document).ready( function() {
 
     //allows the display of the image modification form
     jQuery(".add_the_input_picture").click( function(e) {
-        let name_of_picture = $(this).attr("name");
+        let nameOfPicture = $(this).attr("name");
 
-        jQuery('.new_input_files').append('<div class="preview">\n' +
+        jQuery(".new_input_files").append('<div class="preview">\n' +
             '    <p class="alert alert-warning">Aucun fichier sélectionné pour le moment,' +
             ' attention seul les images en .jpg, .jpeg, .png et d\'une taille inférieur à 2mo sont autorisés!</p>\n' +
             '  </div><div class="custom-file">' +
             '<input type="file" ' +
             'id="input_new_file" ' +
             'class="input_new_file custom-file-input" ' +
-            'name="'+ name_of_picture +'" accept=".jpg, .jpeg, .png" >' +
+            'name="'+ nameOfPicture +'" accept=".jpg, .jpeg, .png" >' +
             '<label class="custom-file-label" for="input_new_file" data-browse="Choisir">nouveau fichier</label>' +
             '<button id="fileSelect" class="btn btn-warning"> Valider le fichier </button></div>');
 
@@ -25,41 +25,41 @@ jQuery(document).ready( function() {
 
         //allows the display of modified photos
         jQuery("#fileSelect").click( function () {
-            let block_new_input = document.querySelector(".new_input_files");
-            let selected_file = document.querySelector("#input_new_file");
+            let blockNewInput = document.querySelector(".new_input_files");
+            let selectedFile = document.querySelector("#input_new_file");
             let preview = document.querySelector(".preview");
 
-            let name_of_picture_modify = document.querySelector(".img_id_"+name_of_picture+'');
+            let nameOfPictureModify = document.querySelector(".img_id_" + nameOfPicture+'');
 
-            handleFiles(selected_file, preview, name_of_picture_modify, name_of_picture, block_new_input);
+            handleFiles(selectedFile, preview, nameOfPictureModify, nameOfPicture, blockNewInput);
 
             jQuery("#btn_input_img").show();
         });
     });
 
     //function use of the page.
-    function handleFiles(selected_file, preview, name_of_picture_modify, name_of_picture, block_new_input ) {
+    function handleFiles(selectedFile, preview, nameOfPictureModify, nameOfPicture, blockNewInput ) {
         while(preview.firstChild) {
             preview.removeChild(preview.firstChild);
         }
 
-        let files = selected_file.files;
+        let files = selectedFile.files;
         if(files.length === 0) {
-            let para = document.createElement('p');
-            para.textContent = 'Aucun fichier actuellement sélectionné pour le téléchargement';
+            let para = document.createElement("p");
+            para.textContent = "Aucun fichier actuellement sélectionné pour le téléchargement";
             preview.appendChild(para);
         } else if (files[0].size > 2000000) {
-            $('.preview').show();
-            let para = document.createElement('p');
-            para.textContent = 'Désoler votre fichier ne suis pas les critères demandés (jpg, jpeg, png, 2mo).';
+            $(".preview").show();
+            let para = document.createElement("p");
+            para.textContent = "Désoler votre fichier ne suis pas les critères demandés (jpg, jpeg, png, 2mo).";
             para.setAttribute('class', "alert alert-danger");
             preview.appendChild(para);
         } else {
-            while (name_of_picture_modify.firstChild) {
-                name_of_picture_modify.removeChild(name_of_picture_modify.firstChild);
+            while (nameOfPictureModify.firstChild) {
+                nameOfPictureModify.removeChild(nameOfPictureModify.firstChild);
             }
-            while (block_new_input.firstChild) {
-                block_new_input.removeChild(block_new_input.firstChild);
+            while (blockNewInput.firstChild) {
+                blockNewInput.removeChild(blockNewInput.firstChild);
             }
 
             for (let i = 0; i < files.length; i++) {
@@ -72,10 +72,10 @@ jQuery(document).ready( function() {
 
                 let img = document.createElement("img");
                 img.classList.add("obj");
-                img.setAttribute('name', name_of_picture);
+                img.setAttribute('name', nameOfPicture);
                 img.setAttribute('class', "obj col-sm-12");
                 img.file = file;
-                name_of_picture_modify.appendChild(img);
+                nameOfPictureModify.appendChild(img);
 
                 let reader = new FileReader();
                 reader.onload = ( function(aImg) {
@@ -91,9 +91,9 @@ jQuery(document).ready( function() {
     //function allowing photos to be saved by the server
     jQuery("#btn_input_img").click( function(e) {
         e.preventDefault();
-        let image_to_save = document.querySelectorAll(".obj");
-        for (let i = 0; i < image_to_save.length; i++) {
-            fileUpload(image_to_save[i], image_to_save[i].file);
+        let imageToSave = document.querySelectorAll(".obj");
+        for (let i = 0; i < imageToSave.length; i++) {
+            fileUpload(imageToSave[i], imageToSave[i].file);
         }
     });
 
@@ -109,26 +109,26 @@ jQuery(document).ready( function() {
 
             success : function(response){
                 //display an message of success
-                let message_success = document.querySelector('.new_input_files');
-                let para = document.createElement('p');
+                let messageSuccess = document.querySelector(".new_input_files");
+                let para = document.createElement("p");
                 para.textContent = response;
                 para.setAttribute('class', "alert alert-success");
-                message_success.appendChild(para);
-                jQuery('#btn_input_img').hide();
+                messageSuccess.appendChild(para);
+                jQuery("#btn_input_img").hide();
                 setTimeout( function () {
-                    message_success.removeChild(message_success.firstChild)
+                    messageSuccess.removeChild(messageSuccess.firstChild)
                 }, 5000);
             },
             error : function(response, status){
                 //displays an error message
-                let message_success = document.querySelector('.new_input_files');
-                let para = document.createElement('p');
+                let messageError = document.querySelector(".new_input_files");
+                let para = document.createElement("p");
                 para.textContent = response + ' : ' + status;
                 para.setAttribute('class', "alert alert-danger");
-                message_success.appendChild(para);
-                jQuery('#btn_input_img').hide();
+                messageError.appendChild(para);
+                jQuery("#btn_input_img").hide();
                 setTimeout( function () {
-                    message_success.removeChild(message_success.firstChild)
+                    messageError.removeChild(messageError.firstChild)
                 }, 5000);
             },
         });
