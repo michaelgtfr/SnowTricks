5f571087e31c48cb8869bfec5c6c7b8c71f51495
mickdu62 <mickdu62200@gmail.com>
2020-01-17 15:33:22 +0100

    modification in deletePictureAsynchronous.js elements request by Codacy

--- public/js/functionJs/deletePictureAsynchronous.js
+++ public/js/functionJs/deletePictureAsynchronous.js
@@ -1,45 +1,45 @@
 //code allowing images to be deleted asynchronously via ajax
 jQuery(document).ready( function() {
-    jQuery('.delete_the_picture').click( function(e) {
-        e.preventDefault();
-        let name_of_picture = $(this).attr('name');
-        let picture_delete = document.querySelector('.img_id_'+name_of_picture);
-        deletePicture(name_of_picture);
-
-        $(picture_delete).parent().remove();
-    });
-
-    function deletePicture(name_picture) {
+    function deletePicture(namePicture) {
         $.ajax({
-            url : '/deletedPicture',
-            type : 'POST',
-            data : 'name=' + name_picture,
-            dataType : 'text',
+            url : "/deletedPicture",
+            type : "POST",
+            data : "name=" + namePicture,
+            dataType : "text",
 
             success : function(response){
                 //display an message of success
-                let message_success = document.querySelector('.new_input_files');
-                let para = document.createElement('p');
+                let messageSuccess = document.querySelector(".new_input_files");
+                let para = document.createElement("p");
                 para.textContent = response;
-                para.setAttribute('class', "alert alert-success");
-                message_success.appendChild(para);
-                jQuery('#btn_input_img').hide();
+                para.setAttribute("class", "alert alert-success");
+                messageSuccess.appendChild(para);
+                jQuery("#btn_input_img").hide();
                 setTimeout( function () {
-                    message_success.removeChild(message_success.firstChild)
+                    messageSuccess.removeChild(messageSuccess.firstChild)
                 }, 5000);
             },
             error : function(response, status){
                 //displays an error message
-                let message_error = document.querySelector('.new_input_files');
-                let para = document.createElement('p');
-                para.textContent = response + ' : ' + status;
-                para.setAttribute('class', "alert alert-danger");
-                message_error.appendChild(para);
-                jQuery('#btn_input_img').hide();
+                let messageError = document.querySelector(".new_input_files");
+                let para = document.createElement("p");
+                para.textContent = response + " : " + status;
+                para.setAttribute("class", "alert alert-danger");
+                messageError.appendChild(para);
+                jQuery("#btn_input_img").hide();
                 setTimeout( function () {
-                    message_error.removeChild(message_error.firstChild)
+                    messageError.removeChild(messageError.firstChild)
                 }, 5000);
             },
         });
     }
+
+    jQuery(".delete_the_picture").click( function(e) {
+        e.preventDefault();
+        let nameOfPicture = $(this).attr("name");
+        let pictureDelete = document.querySelector(".img_id_"+nameOfPicture);
+        deletePicture(nameOfPicture);
+
+        $(pictureDelete).parent().remove();
+    });
 });
