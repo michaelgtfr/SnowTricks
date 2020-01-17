a5a0ea570a121bb7e984eeb3f2c8b28ce17ac475
mickdu62 <mickdu62200@gmail.com>
2020-01-17 15:22:36 +0100

    modification in the deleteLinkMovieAsynchronous.js the elements request by codacy

--- public/js/functionJs/deleteLinkMovieAsynchronous.js
+++ public/js/functionJs/deleteLinkMovieAsynchronous.js
@@ -1,44 +1,44 @@
 //function allowing the link to be deleted to be sent to the server
 jQuery(document).ready( function() {
-    jQuery('.delete_the_link').click(function (e) {
-        e.preventDefault();
-        let name_of_link = $(this).attr('name');
-        let link_delete = document.querySelector('.link_id_' + name_of_link);
-        new DeleteMovie(name_of_link);
-
-        $(link_delete).parent().remove();
-    });
-
-    function DeleteMovie(link_delete) {
+    function DeleteMovie(linkDelete) {
         $.ajax({
-            url : '/deleteMovie',
-            type : 'POST',
-            data : 'name=' + link_delete,
-            dataType : 'text',
+            url : "/deleteMovie",
+            type : "POST",
+            data : "name=" + linkDelete,
+            dataType : "text",
             success : function(response){
                 //display an message of success
-                let message_success = document.querySelector('.new_input_files');
-                let para = document.createElement('p');
+                let messageSuccess = document.querySelector(".new_input_files");
+                let para = document.createElement("p");
                 para.textContent = response;
-                para.setAttribute('class', "alert alert-success");
-                message_success.appendChild(para);
-                jQuery('#btn_input_link').hide();
+                para.setAttribute("class", "alert alert-success");
+                messageSuccess.appendChild(para);
+                jQuery("#btn_input_link").hide();
                 setTimeout( function () {
-                    message_success.removeChild(message_success.firstChild)
+                    messageSuccess.removeChild(messageSuccess.firstChild)
                 }, 5000);
             },
             error : function(response, status){
                 //displays an error message
-                let message_error = document.querySelector('.new_input_files');
-                let para = document.createElement('p');
-                para.textContent = response + ' : ' + status;
-                para.setAttribute('class', "alert alert-danger");
-                message_error.appendChild(para);
-                jQuery('#btn_input_link').hide();
+                let messageError = document.querySelector(".new_input_files");
+                let para = document.createElement("p");
+                para.textContent = response + " : " + status;
+                para.setAttribute("class", "alert alert-danger");
+                messageError.appendChild(para);
+                jQuery("#btn_input_link").hide();
                 setTimeout( function () {
-                    message_error.removeChild(message_error.firstChild)
+                    messageError.removeChild(messageError.firstChild)
                 }, 5000);
             },
         });
     }
+
+    jQuery(".delete_the_link").click(function (e) {
+        e.preventDefault();
+        let nameOfLink = $(this).attr("name");
+        let linkDelete = document.querySelector(".link_id_" + nameOfLink);
+        new DeleteMovie(nameOfLink);
+
+        $(linkDelete).parent().remove();
+    });
 });
