266ac28713c3a3be813b3ccc487e3b0fc97d84c6
mickdu62 <mickdu62200@gmail.com>
2020-02-20 11:44:20 +0100

    modification in the quote in doublequote

--- public/js/functionJs/flangeForInput.js
+++ public/js/functionJs/flangeForInput.js
@@ -2,20 +2,20 @@
 
 jQuery(document).ready( function () {
     function handleInput(e) {
-        let logTitle = document.querySelector('.log_'+e.target.id);
+        let logTitle = document.querySelector(".log_"+e.target.id);
 
-        logTitle.textContent = 'La valeur du champ est de '+ e.target.value.length +' caractères de long' +
-            ' sur '+ e.target.maxLength +' caractères maximum autorisés';
-        logTitle.style.color = '#000000';
+        logTitle.textContent = "La valeur du champ est de "+ e.target.value.length +" caractères de long" +
+            " sur "+ e.target.maxLength +" caractères maximum autorisés";
+        logTitle.style.color = "#000000";
         if (e.target.value.length === e.target.maxLength) {
-            logTitle.textContent = 'vous avez atteint le maximum de caractères autorisé';
-            logTitle.style.color = '#f00'
+            logTitle.textContent = "vous avez atteint le maximum de caractères autorisé";
+            logTitle.style.color = "#f00"
         }
     }
-    let inputTitle = document.querySelector ('#create_article_form_title');
+    let inputTitle = document.querySelector ("#create_article_form_title");
     inputTitle.oninput = handleInput;
 
-    let inputChapo = document.querySelector('#create_article_form_chapo');
+    let inputChapo = document.querySelector("#create_article_form_chapo");
     inputChapo.oninput = handleInput;
 });
 
--- public/js/functionJs/flangerForInputModify.js
+++ public/js/functionJs/flangerForInputModify.js
@@ -2,19 +2,19 @@
 
 jQuery(document).ready( function () {
     function handleInput(e) {
-        let logTitle = document.querySelector('.log_'+e.target.id);
+        let logTitle = document.querySelector(".log_"+e.target.id);
 
-        logTitle.textContent = 'La valeur du champ est de '+ e.target.value.length +' caractères de long' +
-            ' sur '+ e.target.maxLength +' caractères maximum autorisés';
-        logTitle.style.color = '#000000';
+        logTitle.textContent = "La valeur du champ est de "+ e.target.value.length +" caractères de long" +
+            " sur "+ e.target.maxLength +" caractères maximum autorisés";
+        logTitle.style.color = "#000000";
         if (e.target.value.length === e.target.maxLength) {
-            logTitle.textContent = 'vous avez atteint le maximum de caractères autorisé';
-            logTitle.style.color = '#f00'
+            logTitle.textContent = "vous avez atteint le maximum de caractères autorisé";
+            logTitle.style.color = "#f00"
         }
     }
-    let inputTitle = document.querySelector ('#modify_article_form_title');
+    let inputTitle = document.querySelector ("#modify_article_form_title");
     inputTitle.oninput = handleInput;
 
-    let inputChapo = document.querySelector('#modify_article_form_chapo');
+    let inputChapo = document.querySelector("#modify_article_form_chapo");
     inputChapo.oninput = handleInput;
 });
